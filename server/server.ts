9609a22381b1271aa4a79d6f9f42510194f6984b
Quentin Bordelon <qgbordelon@gmail.com>
2025-03-22 13:51:15 -0500

    server

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,9 @@
 import WebSocket from 'ws';
 
-const wss = new WebSocket.Server({ port: 8080 });
+const serverID = (Math.random() + 1).toString(36).substring(5);  
+console.log(serverID);
+
+const wss = new WebSocket.Server({ port: '8080' });
 
 wss.on('connection', (ws: WebSocket) => {
   console.log('new client connected');

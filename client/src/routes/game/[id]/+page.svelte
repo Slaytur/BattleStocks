d2ddb839a0e2d7175840b31ab5ca2016bf0895be
Quentin Bordelon <qgbordelon@gmail.com>
2025-03-22 16:26:39 -0500

    merging game and server create

--- client/src/routes/game/[id]/+page.svelte
+++ client/src/routes/game/[id]/+page.svelte
@@ -3,8 +3,29 @@
 
     const id = $page.params.id;
     import "../client.ts";
+
+    let visibleGame = false;
+    function toggleVisibleGame() {
+      visibleGame = !visibleGame; 
+    };
+
+    let visibleOverlay = true;
+    function toggleOverlay() {
+      visibleOverlay = !visibleOverlay; 
+    };
 </script>
 
+<button on:click={toggleOverlay}>overlay</button>
+<button on:click={toggleGame}>Game</button>
+
+{#if visibleGame}
 <article>
     {id}
 </article>
+{/if}
+
+{#if visibleOverlay}
+<overlay>
+  OVERLAY
+</overlay>
+{/if}

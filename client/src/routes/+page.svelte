389f51f088d8e0d9bf7c3743731815d9ad916704
bennlol <52867429+bennlol@users.noreply.github.com>
2025-03-23 00:45:29 -0500

    sign up + login + splash + header improvements

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,4 +1,29 @@
-<main>
-    <div class="hero"></div>
-    <article></article>
-</main>
+<script>
+    import { onMount } from "svelte";
+    import gsap from "gsap";
+    import img from "$lib/imgs/stock_sheet.jpg";
+    
+    onMount(() => {
+      gsap.to(".animated-img", {
+        duration: 10,
+        y: "-20%", // Moves the image up, creating a pan-down effect
+        scale: 1.1, // Slight zoom-in for a dynamic effect
+        ease: "power1.out",
+      });
+    });
+    </script>
+    
+    <main class="tw:w-full tw:h-screen tw:overflow-hidden tw:relative">
+        <img class="animated-img tw:w-screen tw:mx-auto tw:bg-gray-400 tw:text-center tw:object-cover" 
+             src={img} 
+             alt="stock market graph" />
+        
+             <div class="text-overlay tw:absolute tw:top-0 tw:w-screen tw:h-screen tw:bg-black/40 tw:shadow-xl tw:px-8 tw:py-6 tw:text-center tw:items-center tw:justify-center tw:flex tw:flex-col ">
+                <div class="tw:text-[42px] tw:font-extrabold tw:text-white tw:tracking-wide">
+                    Put Your Trading Skills to the Test with the <span class="tw:text-primary">Ultimate</span>
+                </div>
+                <div class="tw:text-[30px] tw:text-gray-200 tw:mt-3">
+                    Fast-Paced, Competitive, Stock Trading Game
+                </div>
+            </div>
+    </main>

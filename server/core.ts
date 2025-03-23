e6f40f5840300d0e41d74d2056dda02ce824e456
Damien Vesper <ldamienvesper@gmail.com>
2025-03-22 19:59:18 -0500

    yessir

--- server/core.ts
+++
@@ -1,5 +0,0 @@
-import { Logger } from "../shared/src/utils/Logger";
-
-export const core = {
-    logger: new Logger()
-};
---
+++ server/src/core.ts
@@ -0,0 +1,5 @@
+import { Logger } from "../../shared/src/utils/Logger";
+
+export const core = {
+    logger: new Logger()
+};

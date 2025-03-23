93f1618de9881d1be33ab1518e0b8188abbf0cce
Charlie Doan <90109397+charlesbae@users.noreply.github.com>
2025-03-23 11:32:50 -0500

    buttons for events

---
+++ client/src/lib/components/game/Overlay.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+    let ev1 = "event 1";
+    let ev2 = "event 2";
+    let ev3 = "event 3";
+</script>
+
+<section class="card tw:border-[#111211] tw:p-6 tw:flex tw:!flex-row tw:justify-center tw:items-center">
+    <button class="btn btn-lg tw:!mt-4 tw:!bg-secondary tw:!text-white tw:!mx-4">
+        {ev1}
+    </button>
+    <button class="btn btn-lg tw:!mt-4 tw:!bg-secondary tw:!text-white tw:!mx-4">
+        {ev2}
+    </button>
+    <button class="btn btn-lg tw:!mt-4 tw:!bg-secondary tw:!text-white tw:!mx-4">
+        {ev3}
+    </button>
+</section>
+  
